7952b746f83c5ce2c96e46ca22c901040c5edf4b
Dewan <dewan.hasan06gmail.com>
2022-12-03 11:06:31 -0500

    Finished view product (2) & update product (3)

---
+++ Testing/Controllers/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using Testing.Models;
+
+namespace Testing.Controllers
+{
+    internal interface IProductRepository
+    {
+        object GetAllProducts();
+        object GetProduct(int id);
+        void UpdateProduct(Product product);
+    }
+}
---
+++ Testing/IproductRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using Testing.Models;
+namespace Testing
+{
+    public interface IproductRepository
+    {
+        public IEnumerable<Product> GetAllProducts();
+        public Product GetProduct(int id);
+        public void UpdateProduct(Product product);
+    }
+}
