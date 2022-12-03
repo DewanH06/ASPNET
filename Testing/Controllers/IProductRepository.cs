fc2abcbcf758eb434d96ad01f02482de5f2cbb26
Dewan <dewan.hasan06gmail.com>
2022-12-03 15:16:36 -0500

    Delete Product

--- Testing/Controllers/IProductRepository.cs
+++ Testing/Controllers/IProductRepository.cs
@@ -5,6 +5,7 @@ namespace Testing.Controllers
     internal interface IProductRepository
     {
         object AssignCategory();
+        void DeleteProduct(Product product);
         object GetAllProducts();
         object GetProduct(int id);
         void InsertProduct(Product productToInsert);
